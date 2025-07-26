9c1b4f427c52b538301515f64e9ca3bd4e9b67b4
kareem-ic <kclarke11506@gmail.com>
2025-07-26 00:45:25 -0400

    Page routing successfully implemented

---
+++ app/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SignIn = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Sign In Screen</Text>
+    </View>
+  )
+}
+
+export default SignIn
